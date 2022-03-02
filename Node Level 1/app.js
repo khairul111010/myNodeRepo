ac43034cbc8150cf6d46d97b9603372e6551a1bc
Khairul Hasan <khairul18103199@gmail.com>
2022-03-02 21:52:18 +0600

    response url method headers

--- Node Level 1/app.js
+++ Node Level 1/app.js
@@ -1,8 +1,8 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log(req);
-  process.exit();
+  console.log(req.url, req.method, req.headers);
+  //   process.exit();  Hard exits the event loop closes the program
 }); //this function will run for every incoming request
 
 server.listen(3000);
