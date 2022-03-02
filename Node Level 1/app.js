5eb4f906a2fbee18e89d42e50b76ca9667a08dc8
Khairul Hasan <khairul18103199@gmail.com>
2022-03-02 21:47:53 +0600

    process.exit()

--- Node Level 1/app.js
+++ Node Level 1/app.js
@@ -2,6 +2,7 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
   console.log(req);
+  process.exit();
 }); //this function will run for every incoming request
 
 server.listen(3000);
