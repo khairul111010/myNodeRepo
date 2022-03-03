cfd36694ee61aba3fb64caaa7ccefabae5049cd2
khairul111010 <khairul18103199@gmail.com>
2022-03-03 21:48:36 +0600

    filtering starting segment

--- Node-Level-2/app.js
+++ Node-Level-2/app.js
@@ -10,7 +10,7 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(adminRoutes);
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
--- Node-Level-2/routes/admin.js
+++ Node-Level-2/routes/admin.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 router.get("/product", (req, res, next) => {
   res.send(
-    "<form action='/add-product' method='POST'><input type='text' name='product'><button type='submit'>Submit</button></form>"
+    "<form action='/admin/add-product' method='POST'><input type='text' name='product'><button type='submit'>Submit</button></form>"
   );
 });
 router.post("/add-product", (req, res, next) => {
