7b4864dd0433e988a9f2288af9e2d9c2198f9971
khairul111010 <khairul18103199@gmail.com>
2022-03-03 22:17:57 +0600

    using helpers for root directory cleaner way

--- Node-Level-2/routes/admin.js
+++ Node-Level-2/routes/admin.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const path = require("path");
+const rootDir = require("../utils/path");
 
 router.get("/product", (req, res, next) => {
-  res.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
+  res.sendFile(path.join(rootDir, "views", "add-product.html"));
 });
 router.post("/add-product", (req, res, next) => {
   console.log(req.body);
