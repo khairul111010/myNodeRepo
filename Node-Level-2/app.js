5999a56f9bd824de58fe03c6d766dd5a39fff251
khairul111010 <khairul18103199@gmail.com>
2022-03-03 21:40:50 +0600

    should use app.use in index js and spacify get post in router

--- Node-Level-2/app.js
+++ Node-Level-2/app.js
@@ -2,18 +2,15 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const adminRoutes = require("./routes/admin");
-
+const shopRoutes = require("./routes/shop");
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
-  console.log("from middleware");
+  //   console.log("from middleware");
   next();
 });
 
 app.use(adminRoutes);
-
-app.use("/", (req, res, next) => {
-  res.send("Sending req");
-});
+app.use(shopRoutes);
 
 app.listen(3000);
