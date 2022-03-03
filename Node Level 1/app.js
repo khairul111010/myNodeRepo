1bf31b11d23254c76480ee6b8bdb759d6c9647e5
Khairul Hasan <khairul18103199@gmail.com>
2022-03-03 14:07:15 +0600

    url redirect

--- Node Level 1/app.js
+++ Node Level 1/app.js
@@ -2,11 +2,20 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method, req.headers);
+  if (req.url == "/") {
+    res.write("<html>");
+    res.write("<head><title>Title</title></head>");
+    res.write(
+      "<body><form action='/message' method='POST'><input type='text'><button type='submit'>SEND</button></form></body>"
+    );
+    res.write("</html>");
+    return res.end();
+  }
   res.setHeader("Content-Type", "text/html");
-  res.write("<html>");
+  res.write("<htmlhtml>");
   res.write("<head><title>Head</title></head>");
-  res.write("<body><h1>BODY</h1></body>");
-  res.write("<html>");
+  res.write("<body><h1>slash</h1></body>");
+  res.write("</htmlhtml>");
   res.end();
   //   process.exit();  Hard exits the event loop closes the program
 }); //this function will run for every incoming request
